85cef17fd4e3805a69a4c4752689eb3c56cc8c37
David Bushard <davidbushard@gmail.com>
2021-09-06 20:43:14 -0700

    Deploy

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import About from "./components/About/About";
+import Contact from "./components//Contact/Contact";
+import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
+import Testimonials from "./components/Testimonials/Testimonials";
+
+export default function App() {
+  return (
+    <main className="text-gray-200 bg-gray-500 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./style.css";
+
+export default function Navbar() {
+  return (
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="title-font font-medium text-white mb-4 md:mb-0">
+          <a href="#about" className="ml-3 text-xl">
+            David Bushard
+          </a>
+        </a>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <div className="icon-spacing">
+            <h2>GitHub</h2>
+            <a href="">
+              <i class="fab fa-github"></i>
+            </a>
+          </div>
+          <div className="icon-spacing">
+            <h2>Design</h2>
+            <a href="">
+              <i class="fab fa-behance"></i>
+            </a>
+          </div>
+          <div className="icon-spacing">
+            <h2>LinkedIn</h2>
+            <a href="">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+          <div className="icon-spacing">
+            <h2>Instagram</h2>
+            <a href="">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
